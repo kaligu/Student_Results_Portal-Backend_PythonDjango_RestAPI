77b3b7f63858bd52c5be1b2917afd4aaf54b6651
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-02-02 22:12:54 +0530

    implimenting model

--- school/models.py
+++ school/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class School(models.Model):
-    id=models.CharField(max_length=200)
-    name=models.CharField(max_length=200)
+    id = models.CharField(primary_key=True, max_length=200)
+    name = models.CharField(max_length=200)
+
+    def _str_(self):
+        return self.id + ' ' + self.name
