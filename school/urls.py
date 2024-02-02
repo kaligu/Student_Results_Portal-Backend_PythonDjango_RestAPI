31e6096b4e4924995b0c4b0103aa4f3d2fb13b4d
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-02-02 23:19:58 +0530

    implimenting

--- school/urls.py
+++ school/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from school import views
 
 urlpatterns = [
-    path('get-all/', views.get_all_schools, name='school_get_all')
+    path('get-all/', views.get_all_schools, name='school_get_all'),
+    path('add-school/', views.add_school, name='add_school')
 ]
--- school/views.py
+++ school/views.py
@@ -2,8 +2,18 @@ from django.http import HttpResponse
 from .models import School
 from django.core.serializers import serialize
 # from .serializers import SchoolSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
 
 def get_all_schools(request):
     school = School.objects.all()
     # serializer = SchoolSerializer(school, many=True)
     return HttpResponse(" dddd"+serialize('json', school))
+
+@csrf_exempt
+@require_POST
+def add_school(request):
+    data = json.loads(request.body)
+    new_school = School.objects.create(**data)
+    return HttpResponse("Done")
